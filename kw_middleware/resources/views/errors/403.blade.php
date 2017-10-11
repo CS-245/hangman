6a243f6010c72dc5290bc2d790b0f4ce6289e7b2
Lenny Yang <lennyiswise@gmail.com>
2017-10-11 10:27:36 -0700

    rollback

--- kw_middleware/resources/views/errors/403.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        403 Page... Unauthorized action.
-    </div>
-</div>
-@endsection
--- kw_middleware/resources/views/errors/404.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        404 Page...
-    </div>
-</div>
-@endsection
