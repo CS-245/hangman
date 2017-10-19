d2c5012f3fc9e5b1abf479c7f00116cf0724140c
Lenny Yang <lennyiswise@gmail.com>
2017-10-19 11:37:50 -0700

    Converted PaintPanel file to java and class

--- src/hangman/PaintPanel.java
+++ src/hangman/PaintPanel.java
@@ -1,8 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package hangman;
 
 
-import javax.swing.*;
-import java.awt.*;
 import java.awt.BasicStroke;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
